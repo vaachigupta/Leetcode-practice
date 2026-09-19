54ec7bfe52cfce5ec299cfce074864b13607cf69
Vaachi Gupta <149156873+vaachigupta@users.noreply.github.com>
2026-09-19 23:55:17 +0530

    Time: 0 ms (100%), Space: 20.5 MB (42.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        freq={}
+
         for i in range(len(nums)):
             diff=target-nums[i]
-            for j in range(i+1,len(nums)):
-                if nums[j]==diff:
-                    return [i,j]
+
+            if diff in freq:
+                return [freq[diff], i]
+            
+            freq[nums[i]]=i
         
-        return []
